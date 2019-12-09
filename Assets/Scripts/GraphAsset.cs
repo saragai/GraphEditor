c372ceff0184bdabf586d9633d8e03bdd30dd566
saragai <takanori.saragai@angoo.co.jp>
2019-12-09 18:36:10 +0900

    Add: graphAsset

---
+++ Assets/Scripts/GraphAsset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu( fileName = "graph.graph.asset", menuName = "Graph/Graph" )]
+public class GraphAsset: ScriptableObject
+{
+	public List<SerializableNode> nodes = new List<SerializableNode>();
+}
+
+[Serializable]
+public class SerializableNode
+{
+	public Vector2 position;
+	public List<SerializableEdge> edges;
+}
+
+[Serializable]
+public class SerializableEdge
+{
+	public int nodeId;
+}
